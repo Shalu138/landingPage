3b9c18d9b03bed5523004c052b1991c3d8ae0ad6
Shalu <shalu.handysolver@gmail.com>
2023-01-12 12:22:05 +0530

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+$('#client-slider').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    // speed: 1000,
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 10000,
+    pauseOnHover: false,
+    cssEase: 'linear',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 5,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
